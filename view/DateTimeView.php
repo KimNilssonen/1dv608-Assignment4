e3ea980f6000fbd7d5fcbc226f657c3a20cec6ed
kn222gp <kn222gp@student.lnu.se>
2015-09-09 11:34:35 +0000

    Added servertime

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -5,7 +5,14 @@ class DateTimeView {
 
 	public function show() {
 
-		$timeString = 'TODO, Write servertime here...';
+
+		date_default_timezone_set("Europe/Stockholm");
+		$serverTime = getdate();
+
+		// Had it like this before. Discussed a better solution that just includes one 'date()', which is now the $timeString variable.
+		//$timeString = date("l") . ', the ' . date("j") . ' of ' . date("F") . ', The time is ' . date("H:i:s");
+		
+		$timeString = date('l\, \t\h\e jS \o\f F Y, \T\h\e \t\i\m\e \i\s H:i:s');
 
 		return '<p>' . $timeString . '</p>';
 	}
